0113188b6ca5746af2ead18de1ee3b676e1d0765
Daniela10289 <dpalacio2200@gmail.com>
2021-10-25 14:25:44 -0500

    Custom Hooks para la tienda

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,23 +1,18 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import ProductItem from '../components/ProductItem';
+import useGetProducts from '../hooks/useGetProducts';
 import '@styles/ProductList.scss';
-import axios from 'axios';
 
 const API = 'http://api.escuelajs.co/api/v1/products';
 
 const ProductList = () => {
-	const [products, setProducts] = useState([]);
-
-	useEffect(async ()=> {
-		const response = await axios(API);
-		setProducts(response.data);
-	}, [])	
-
+	const products = useGetProducts(API);
 	return (
 		<section className="main-container">
 			<div className="ProductList">
 				{products.map(product => (
-					<ProductItem />	//se va iterar cada de uno de los productos de products y hacer render de los product item
+					//se va iterar cada de uno de los productos de products y hacer render de los product item
+					<ProductItem product={product} key={product.id}/>	
 				))}
 			</div>
 		</section>
